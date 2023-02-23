4aaed94d80967a739c74f5eca43ef82d2f961ce0
Chalita Jodrum <chalita0218@gmail.com>
2023-02-23 14:06:52 +0700

    แก้migration

--- database/migrations/2023_01_31_143141_create_complaints_table.php
+++ database/migrations/2023_01_31_143141_create_complaints_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('status')->default(0);
             $table->string('pb_id');
             $table->string('cn_id');
+            $table->string('ag_id');
             $table->timestamps();
         });
     }
